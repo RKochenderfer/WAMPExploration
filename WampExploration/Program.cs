eb3d16e2b2977628924e1405edb0c5c4192426ac
Raymond Kochenderfer <Raymond_Kochenderfer@uhaul.com>
2024-06-28 15:04:58 -0400

    Add publisher and consumer

--- WampExploration/Program.cs
+++ WampExploration/Program.cs
@@ -1,4 +1,5 @@
-﻿using WampExploration;
+﻿using System.Reactive.Linq;
+using WampExploration;
 using WampSharp.V2;
 
 const string location = "ws://127.0.0.1:8080/ws";
@@ -12,4 +13,22 @@ await realm.Services.RegisterCallee(argumentsService);
 host.Open();
 
 Console.WriteLine($"Server is running on {location}");
+
+var subject = realm.Services.GetSubject<int>("com.pubsub.topic1");
+var counter = 0;
+var timer = Observable.Timer(TimeSpan.FromMicroseconds(0), TimeSpan.FromMilliseconds(1000));
+
+IDisposable disposable = timer.Subscribe(x => 
+{
+    counter++;
+    Console.WriteLine("Publishing to topic 'com.myapp.topic1': " + counter);
+    try 
+    {
+        subject.OnNext(counter);
+    } catch (Exception ex)
+    {
+        Console.WriteLine(ex.ToString());
+    }
+});
+
 Console.ReadLine();
--- frontend/index.js
+++ frontend/index.js
@@ -9,10 +9,17 @@ const realm = 'realm1'
 const connection = new autobahn.Connection({ url: url, realm: realm })
 
 connection.onopen = async (session) => {
+    // caller callee example
     pingSelector.onclick = async () => {
         const result = await session.call('com.arguments.ping', [])
-        responseListSelector.innerHTML = `${responseListSelector.innerHTML}<li class='list-group-item'>${result}</li>`
+        responseListSelector.innerHTML += `<li class='list-group-item'>${result}</li>`
     }
+
+    // pub sub example
+    session.subscribe('com.pubsub.topic1', (args) => {
+        pubSubListSelector.innerHTML += `<li class='list-group-item'>${args[0]}</li>`
+    })
+    
 
     console.log(`Connected`)
 } 
